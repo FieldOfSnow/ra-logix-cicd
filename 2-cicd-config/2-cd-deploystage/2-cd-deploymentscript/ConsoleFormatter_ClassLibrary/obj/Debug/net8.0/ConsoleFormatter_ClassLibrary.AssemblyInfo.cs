1026bfdd9fe9f0c6419f8ffcbff9f93a79618ddc
Peter <peter.gent@na.denso.com>
2025-04-02 14:25:21 -0400

    Installed EPPLus and rebuilt

--- 2-cicd-config/2-cd-deploystage/2-cd-deploymentscript/ConsoleFormatter_ClassLibrary/obj/Debug/net8.0/ConsoleFormatter_ClassLibrary.AssemblyInfo.cs
+++ 2-cicd-config/2-cd-deploystage/2-cd-deploymentscript/ConsoleFormatter_ClassLibrary/obj/Debug/net8.0/ConsoleFormatter_ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleFormatter_ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426a848961519a1533551df35c1d939b2dc3712d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127c3fd9dcc90c8edd052c54dc71a682179b7663")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleFormatter_ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleFormatter_ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
